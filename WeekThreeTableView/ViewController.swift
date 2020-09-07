a09328bd60fe0d47855b953abdb8ae573df7bce6
Vital Sowath <vitalsowath@gmail.com>
2020-09-07 11:08:20 +0700

    Initial Commit

---
+++ WeekThreeTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekThreeTableView
+//
+//  Created by Sowath Vital on 9/7/20.
+//  Copyright © 2020 Codingate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
